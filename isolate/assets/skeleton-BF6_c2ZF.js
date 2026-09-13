0698bf3ffb32b3ec2bf9c6a99dc01b69112b11fc
Barry King <237234850+hakosworld-blip@users.noreply.github.com>
2026-09-13 07:30:28 +0000

    Before: Make a real and legal privacy policy based on all the data that is being collected in the website. M...

--- isolate/assets/skeleton-BF6_c2ZF.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./framer-motion-6UB2cudr.js";import{c as s}from"./button-jvrtqrx4.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
---
+++ isolate/assets/skeleton-D3ekwIG8.js
@@ -0,0 +1,1 @@
+import{j as o}from"./framer-motion-6UB2cudr.js";import{c as s}from"./button-B7ntaoQn.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
